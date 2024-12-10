98f7e81e96385cebafa9d9069122242949e59abd
Maxime Riaud <maxime.riaud@datadoghq.com>
2024-12-10 15:23:51 +0100

    update grpc GetDDAgentClient to check IPC server certificate

--- pkg/process/checks/host_info.go
+++ pkg/process/checks/host_info.go
@@ -8,6 +8,7 @@ package checks
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
 	"fmt"
 	"os"
 	"os/exec"
@@ -17,6 +18,7 @@ import (
 	model "github.com/DataDog/agent-payload/v5/process"
 	"google.golang.org/grpc"
 
+	"github.com/DataDog/datadog-agent/pkg/api/util"
 	pkgconfigmodel "github.com/DataDog/datadog-agent/pkg/config/model"
 	pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
 	pb "github.com/DataDog/datadog-agent/pkg/proto/pbgo/core"
@@ -144,7 +146,7 @@ func getHostnameFromCmd(ddAgentBin string, cmdFn cmdFunc) (string, error) {
 }
 
 // getHostnameFromGRPC retrieves the hostname from the main datadog agent via GRPC
-func getHostnameFromGRPC(ctx context.Context, grpcClientFn func(ctx context.Context, address, port string, opts ...grpc.DialOption) (pb.AgentClient, error), grpcConnectionTimeout time.Duration) (string, error) {
+func getHostnameFromGRPC(ctx context.Context, grpcClientFn func(ctx context.Context, address, port string, tlsConfig func() *tls.Config, opts ...grpc.DialOption) (pb.AgentClient, error), grpcConnectionTimeout time.Duration) (string, error) {
 	ctx, cancel := context.WithTimeout(ctx, grpcConnectionTimeout)
 	defer cancel()
 
@@ -153,7 +155,11 @@ func getHostnameFromGRPC(ctx context.Context, grpcClientFn func(ctx context.Cont
 		return "", err
 	}
 
-	ddAgentClient, err := grpcClientFn(ctx, ipcAddress, pkgconfigsetup.GetIPCPort())
+	err = util.SetAuthToken(pkgconfigsetup.Datadog())
+	if err != nil {
+		return "", err
+	}
+	ddAgentClient, err := grpcClientFn(ctx, ipcAddress, pkgconfigsetup.GetIPCPort(), util.GetTLSClientConfig)
 	if err != nil {
 		return "", fmt.Errorf("cannot connect to datadog agent via grpc: %w", err)
 	}
--- pkg/util/grpc/agent_client.go
+++ pkg/util/grpc/agent_client.go
@@ -28,15 +28,12 @@ var defaultBackoffConfig = backoff.Config{
 	MaxDelay:   2 * time.Second,
 }
 
-func getGRPCClientConn(ctx context.Context, ipcAddress string, cmdPort string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+func getGRPCClientConn(ctx context.Context, ipcAddress string, cmdPort string, tlsConfigGetter func() *tls.Config, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 	if cmdPort == "-1" {
 		return nil, errors.New("grpc client disabled via cmd_port: -1")
 	}
 
-	// This is needed as the server hangs when using "grpc.WithInsecure()"
-	tlsConf := tls.Config{InsecureSkipVerify: true}
-
-	opts = append(opts, grpc.WithTransportCredentials(credentials.NewTLS(&tlsConf)))
+	opts = append(opts, grpc.WithTransportCredentials(credentials.NewTLS(tlsConfigGetter())))
 
 	target := net.JoinHostPort(ipcAddress, cmdPort)
 
@@ -52,11 +49,11 @@ var defaultAgentClientDialOpts = []grpc.DialOption{
 
 // GetDDAgentClient creates a pb.AgentClient for IPC with the main agent via gRPC. This call is blocking by default, so
 // it is up to the caller to supply a context with appropriate timeout/cancel options
-func GetDDAgentClient(ctx context.Context, ipcAddress string, cmdPort string, opts ...grpc.DialOption) (pb.AgentClient, error) {
+func GetDDAgentClient(ctx context.Context, ipcAddress string, cmdPort string, tlsConfigGetter func() *tls.Config, opts ...grpc.DialOption) (pb.AgentClient, error) {
 	if len(opts) == 0 {
 		opts = defaultAgentClientDialOpts
 	}
-	conn, err := getGRPCClientConn(ctx, ipcAddress, cmdPort, opts...)
+	conn, err := getGRPCClientConn(ctx, ipcAddress, cmdPort, tlsConfigGetter, opts...)
 	if err != nil {
 		return nil, err
 	}
